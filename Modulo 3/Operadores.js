ca433e95f359bdfd3c17561e00f059966d87921f
Ale Miranda <alemiranda2511@gmail.com>
2023-01-25 01:05:35 -0300

    Operadores

---
+++ Modulo 3/Operadores.js
@@ -0,0 +1,12 @@
+// Operadores Numéricos
+console.log("Suma de 3 + 5: ", 3 + 5);
+console.log("Resta de 17 - 7: ", 14 - 7 );
+console.log("Multiplicacion de 3 * 2: ", 3 * 2);
+console.log("Division de 40/5: ", 40 / 5);
+console.log("Módulo de 95%4:", 95 % 4);
+console.log("Resultado de operación 3 + 5 * 2 - 8 es:", 3+5*2-8)
+// Operadores de adicion de cadenas o strings
+console.log('Hola ' + 'Ale');
+var nombre = 'Ale'
+var saludo = 'Hola ' + nombre + ' como estas?';
+console.log(saludo);
