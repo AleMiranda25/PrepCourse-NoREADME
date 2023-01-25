93c2c57ff1a19e71715d63d6522ae2ceacbfc326
Ale Miranda <alemiranda2511@gmail.com>
2023-01-24 21:04:14 -0300

    "Actualizado Modulo 3"

---
+++ Modulo 3/Variables.js
@@ -0,0 +1,14 @@
+//Strings
+var bootcamp = 'Henry';
+//Numeros
+var numEntero = 5;
+var numNegativo = -10;
+var numDecimal = 2.5;
+//Booleano
+var estoyEntendiendo = true;
+var estoyAburrido = false;
+//Undefined
+var cajon1;
+console.log(cajon1);
+//Null
+var vacio = null;
