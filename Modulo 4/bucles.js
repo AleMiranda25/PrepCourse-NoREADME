25d17290166aae500deec89374ca371d2205e203
Ale Miranda <alemiranda2511@gmail.com>
2023-01-26 18:20:11 -0300

    Modulo 4

---
+++ Modulo 4/bucles.js
@@ -0,0 +1,17 @@
+// Bucle FOR
+var suma = 0;
+
+/*
+for (let i = 0; i < 5 ; i++) {
+    suma = suma + i;
+    console.log("Variable de iteracion: ", i);    
+}
+console.log("Variable suma: ", suma);  
+*/
+
+// Bucle WHILE
+
+while (suma < 3) {
+    suma = suma + 1;
+    console.log(suma);
+}
