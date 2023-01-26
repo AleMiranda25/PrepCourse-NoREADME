2c794a825dc4c64f6230ca05fb8cc50106fbe5e8
Ale Miranda <alemiranda2511@gmail.com>
2023-01-26 16:13:17 -0300

    Fin Modulo 3

--- Modulo 3/controlDeFlujos.js
+++ Modulo 3/controlDeFlujos.js
@@ -3,8 +3,18 @@ function viajar (destino) {
         console.log("Gire a la Izquierda");
     } else if(destino === "Argentina") {
         console.log("Gire a la Derecha");
-    } 
+    } else console.log("Te has perdido");
 }
 
 //viajar("Brasil");
-viajar("Argentina");
+//viajar("Argentina");
+viajar("Uruguay");
+
+function puedeManejar (edad) {
+    if(edad >= 18){
+        console.log("Puede Manejar!");
+    } else console.log("No puede Manejar!");
+}
+
+puedeManejar(25);
+//puedeManejar(16);
